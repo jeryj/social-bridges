e3dd38fddb8b6f7efb4832003c77e27b72c84982
Jerry <jones.jeremydavid@gmail.com>
2016-09-13 22:21:30 -0500

    initial commit with base file structure

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='UTF-8'>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <title>Social Bridges</title>
+
+    <link rel='stylesheet' href='/dist/style.css' />
+    <script type='text/javascript' src='/dist/scripts.js'></script>
+</head>
+
+<body>
+
+<header role='banner'>
+    <h1><a href='/'>Social Bridges</a></h1>
+    <nav role='navigation'>
+        <ul>
+            <li><a href='#location'>Start Assessment</a></li>
+        </ul>
+    </nav>
+</header>
+
+<main>
+    <div class='hero'>
+        <h2>About this?</h2>
+    </div>
+</main>
+
+
+<footer>
+
+</footer>
+
+</body>
+</html>
