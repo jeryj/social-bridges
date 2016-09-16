09bc7ba4ee3784373f823e519682e7d7d6c51544
Jerry <jones.jeremydavid@gmail.com>
2016-09-15 23:03:25 -0500

    moved svg to dist

--- views/header.php
+++ views/header.php
@@ -1,4 +1,4 @@
 <header role='banner'>
-    <?php echo file_get_contents('assets/img/bridge.svg');?>
+    <?php echo file_get_contents('dist/img/bridge.svg');?>
     <h2><a href='index.php'><svg><use xlink:href="#bridge" /></svg>Social Bridges</a></h2>
 </header>
