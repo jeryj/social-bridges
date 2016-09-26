e954730e2e55c75f8a93cca29ab49c690487de60
Jerry <jones.jeremydavid@gmail.com>
2016-09-26 14:12:27 -0500

    color and homepage content changes

--- index.php
+++ index.php
@@ -16,12 +16,13 @@
 <body>
 <?php include('views/header.php');?>
 <main>
-    <div class="hero">
+    <section class="hero">
         <div class="container">
-            <h1 class="ad__title">Assess Social Comrehension</h1>
+
             <div id="question-container" class="well">
-                <fieldset id="question" aria-role="presentation">
-                    <legend><span class="sr">Example Question: </span>Who is happy?</legend>
+                <figure id="question" aria-role="presentation">
+                    <figcaption><span class="sr">Example question from the Social Bridges Assessment</sr></figcaption>
+                    <div class="question__text">Who is happy?</div>
 
                         <label class="exp">
                             <img class="face" src="dist/img/happy.jpg" alt="" />
@@ -31,50 +32,54 @@
                             <img class="face" src="dist/img/distracted.jpg" alt="" />
                             <span class="sr">Face with slanted eyebrows and small eyes. Flared nostrils with mouth open wide as if screaming, showing teeth.</span>
                         </label>
-                </fieldset>
-                <a class="overlay-link" href='assessment.php'></a>
-                <p class="assessment-link__container">
-                    <a class="assessment-link btn" href='assessment.php'>Start Assessment</a>
-                </p>
-            </div>
-        </div>
-    </div>
+                </figure>
 
-    <div class="report-container">
+                <a class="overlay-link" href='assessment.php'></a>
+            </div>
+            <h1 class="ad__title">Autism Social Comrehension Assessment</h1>
+            <p class="ad__description">Used by Speech-Language Pathologists as a pre-screening &amp; therapy practice tool.</p>
+            <p class="ad__link-container">
+                <a class="ad__link btn" href='assessment.php'>Start Assessment</a>
+            </p>
+        </div>
+    </section>
+
+    <section class="report-container">
 
         <div class="container">
-            <h2 class="ad__title">Get Detailed Results</h2>
-            <div class="results well">
-                    <div class="result">
-                <h3 class="results__title">Phrase</h3>
-                <p class="results__description">"Who is..." type questions</p>
-                <div class="score score--breakdown">
-                    <p class="score__title">100<span class="score__percentage">%</span><span class="score__fraction">4 of 4<span class="sr"> Correct</span></span></p>
-                    <svg class="score-circle" width="200" height="200" viewport="0 0 100 100" version="1.1" xmlns="http://www.w3.org/2000/svg">
-                      <circle class="score-circle__bg" r="90" cx="100" cy="100" fill="transparent" stroke-dasharray="565.48" stroke-dashoffset="0"></circle>
-                      <circle class="score-circle__path score-circle__setOffset" r="90" cx="100" cy="100" fill="transparent" stroke-dasharray="565.48" stroke-dashoffset="0"></circle>
-                    </svg>
+            <figure class="results well">
+                <div class="result">
+                    <h3 class="results__title">Phrase</h3>
+                    <p class="results__description">"Who is..." type questions</p>
+                    <div class="score score--breakdown">
+                        <p class="score__title">100<span class="score__percentage">%</span><span class="score__fraction">4 of 4<span class="sr"> Correct</span></span></p>
+                        <svg class="score-circle" width="200" height="200" viewport="0 0 100 100" version="1.1" xmlns="http://www.w3.org/2000/svg">
+                          <circle class="score-circle__bg" r="90" cx="100" cy="100" fill="transparent" stroke-dasharray="565.48" stroke-dashoffset="0"></circle>
+                          <circle class="score-circle__path score-circle__setOffset" r="90" cx="100" cy="100" fill="transparent" stroke-dasharray="565.48" stroke-dashoffset="0"></circle>
+                        </svg>
+                    </div>
                 </div>
-            </div>
-            <div class="result">
-                <h3 class="results__title">Application</h3>
-                <p class="results__description">Scenario type questions.</p>
-                <div class="score score--breakdown">
-                    <p class="score__title">75<span class="score__percentage">%</span><span class="score__fraction">3 of 4<span class="sr"> Correct</span></span></p>
-                    <svg class="score-circle" width="200" height="200" viewport="0 0 100 100" version="1.1" xmlns="http://www.w3.org/2000/svg">
-                      <circle class="score-circle__bg" r="90" cx="100" cy="100" fill="transparent" stroke-dasharray="565.48" stroke-dashoffset="0"></circle>
-                      <circle class="score-circle__path score-circle__setOffset" r="90" cx="100" cy="100" fill="transparent" stroke-dasharray="565.48" stroke-dashoffset="141.371669412"></circle>
-                    </svg>
+                <div class="result">
+                    <h3 class="results__title">Application</h3>
+                    <p class="results__description">Scenario type questions.</p>
+                    <div class="score score--breakdown">
+                        <p class="score__title">75<span class="score__percentage">%</span><span class="score__fraction">3 of 4<span class="sr"> Correct</span></span></p>
+                        <svg class="score-circle" width="200" height="200" viewport="0 0 100 100" version="1.1" xmlns="http://www.w3.org/2000/svg">
+                          <circle class="score-circle__bg" r="90" cx="100" cy="100" fill="transparent" stroke-dasharray="565.48" stroke-dashoffset="0"></circle>
+                          <circle class="score-circle__path score-circle__setOffset" r="90" cx="100" cy="100" fill="transparent" stroke-dasharray="565.48" stroke-dashoffset="141.371669412"></circle>
+                        </svg>
+                    </div>
                 </div>
-            </div>
-    </div>
-</div>
-
-<p class="restart-assessment"><a class="btn" href="assessment.php">Start Assessment</a></p>
-</main>
+            </figure>
+            <h2 class="ad__title">Detailed Results</h2>
+            <p class="ad__description">Get a full-picture of your student's in a glance.</p>
+            <p class="ad__link-container"><a class="btn" href="assessment.php">Start Assessment</a></p>
         </div>
-    </div>
+    </section>
 
 </main>
+<footer>
+    <p>Social Bridges &copy; <?php echo Date('Y');?>
+</footer>
 </body>
 </html>
